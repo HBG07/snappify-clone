0ea9724d2b5c44b7967bb4d261e7eed2f7cbc1f0
Hamsel Brea García <hamselb24@gmail.com>
2023-02-15 13:31:31 -0500

    add prettier-plugin-tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
+import Head from "next/head";
+import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -13,8 +13,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className='text-3xl font-bold underline'>Hello World</h1>
+        <h1 className="text-3xl font-bold underline">Hello World</h1>
       </main>
     </>
-  )
+  );
 }
